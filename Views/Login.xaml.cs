f602936da9715abd8c376c89d43f62f6e6e8e7ef
AselaDK <aselax2@gmail.com>
2019-10-01 23:15:43 +0530

    login has improved, Password was hashed(md5). Login help message was added, Wrong password/uname message was corrected, Disabled login button till fill the textboxes

--- Views/Login.xaml.cs
+++ Views/Login.xaml.cs
@@ -16,6 +16,7 @@ using Amazon.DynamoDBv2;
 using Amazon.DynamoDBv2.DocumentModel;
 using static Amazon.Internal.RegionEndpointProviderV2;
 using Table = Amazon.DynamoDBv2.DocumentModel.Table;
+using System.Security.Cryptography;
 
 namespace Activator.Views
 {
@@ -33,9 +34,10 @@ namespace Activator.Views
         {
             String aId = txtuid.Text;
             String aPassword = txtpassword.Password;
+            String hashPassword = MD5Hash(aPassword);
 
-            Console.WriteLine(aId);
-            Console.WriteLine(aPassword);
+            //Console.WriteLine(aId);
+            //Console.WriteLine(aPassword);
 
             try
             {
@@ -46,18 +48,11 @@ namespace Activator.Views
                 var table = Table.LoadTable(client, tableName);
                 var item = table.GetItem(aId);
 
-                Console.WriteLine(item["aPassword"]);
+                //Console.WriteLine(item["aPassword"]);
 
-                bool count = false;      
-                
-                if(item["aPassword"] == aPassword)
+                if (item!=null && item["aPassword"] == hashPassword)
                 {
-                    count = true;
-                }
-
-                if (count == true)
-                {
-                    Console.WriteLine("Hello World!");
+                    Console.WriteLine("Successfully Logged in!!!");
                     MainWindow dashboard = new MainWindow();
                     dashboard.Show();
                     this.Close();
@@ -65,23 +60,54 @@ namespace Activator.Views
                 else
                 {
                     MessageBox.Show("Username or Password is incorrect!");
+
+                    //clear texboxes
+                    txtuid.Text = "";
+                    txtuid.BorderBrush = Brushes.Red;
+                    //txtuid.Background = Brushes.LightSalmon;
+                   
+                    txtpassword.Password = "";
+                    txtpassword.BorderBrush = Brushes.Red;
+                    //txtpassword.Background = Brushes.LightSalmon;
                 }
 
     
             }
             catch (AmazonDynamoDBException ex)
             {
-                MessageBox.Show(ex.Message);
+                MessageBox.Show("Message : Server Error", ex.Message);
             }
             catch (Exception ex)
             {
-                MessageBox.Show(ex.Message);
+                MessageBox.Show("Message : Unknown Error", ex.Message);
             }
         }
 
         private void BtnForget_Click(object sender, RoutedEventArgs e)
         {
+            MessageBox.Show("Please Contact the Developer Team. Thank You!");
+        }
 
+        //encrypter password
+        public static string MD5Hash(string text)
+        {
+            MD5 md5 = new MD5CryptoServiceProvider();
+
+            //compute hash from the bytes of text  
+            md5.ComputeHash(ASCIIEncoding.ASCII.GetBytes(text));
+
+            //get hash result after compute it  
+            byte[] result = md5.Hash;
+
+            StringBuilder strBuilder = new StringBuilder();
+            for (int i = 0; i < result.Length; i++)
+            {
+                //change it into 2 hexadecimal digits  
+                //for each byte  
+                strBuilder.Append(result[i].ToString("x2"));
+            }
+
+            return strBuilder.ToString();
         }
     }
 }
