8cd4b173a50943d1a14118702c333d274e1b4b04
PAMALSAHAN <p.s.jayawickrama@gmail.com>
2019-11-19 00:56:35 +0530

    design add new ref

--- Activator/Views/ReadersView.xaml.cs
+++ Activator/Views/ReadersView.xaml.cs
@@ -10,7 +10,6 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace Activator.Views
@@ -18,7 +17,7 @@ namespace Activator.Views
     /// <summary>
     /// Interaction logic for ReadersView.xaml
     /// </summary>
-    public partial class ReadersView : UserControl
+    public partial class ReadersView : Window
     {
         public ReadersView()
         {
