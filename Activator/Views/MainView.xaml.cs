56b86d71ec29ea82f15dad009531e44bb3b52ec6
tharinduprabath <tharindurulz.mc@gmail.com>
2019-12-25 20:11:35 +0530

    Camera View redesign Add new camera with create video stream, create stream processor, create dynamodb record need to implement create data stream, add new trigger to the lambda function

--- Activator/Views/MainView.xaml.cs
+++ Activator/Views/MainView.xaml.cs
@@ -125,26 +125,26 @@ namespace Activator.Views
 
         private void ButtonMenuCameras_Click(object sender, RoutedEventArgs e)
         {
-            CamerasPageView cams = new CamerasPageView();
-            MenuPage.Content = cams;
+            CameraView cameraView = new CameraView();
+            MenuPage.Content = cameraView;
             lblTitle.Content = "CAMERAS";
             
         }
 
         private void CheckStreamProcessorStatus()
         {
-            Mouse.OverrideCursor = Cursors.Wait;
-            try
-            {
-                if (!Models.Starter.ListStreamProcessors().Contains(Models.MyAWSConfigs.streamProcessorName))
-                {
-                    Models.Starter.CreateStreamProcessor();
-                }
-            }
-            finally
-            {
-                Mouse.OverrideCursor = null;
-            }
+            //Mouse.OverrideCursor = Cursors.Wait;
+            //try
+            //{
+            //    if (!Models.Starter.ListStreamProcessors().Contains(Models.MyAWSConfigs.StreamProcessorName))
+            //    {
+            //        Models.Starter.CreateStreamProcessor();
+            //    }
+            //}
+            //finally
+            //{
+            //    Mouse.OverrideCursor = null;
+            //}
         }
 
         // function for check a window is open & avoid opening it twice
